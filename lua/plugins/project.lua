e063d41b428dcc2509df4149be48715c0db34e7d
MUHAMMAD UMAR SAEED <musaeed178@gmail.com>
2026-08-26 19:55:06 +0500

    Update project.lua

---
+++ lua/plugins/project.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    event = "VeryLazy",
+    opts = {
+      manual_mode = true,
+    },
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("projects")
+      end)
+    end,
+  },
+}
